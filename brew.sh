5f17b36c8eec7187e68a1e9ca27126d2e867fc9f
IsotopeZ <gcptien@gmail.com>
2020-06-02 15:32:25 +0800

    Init

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install microsoft-office
+brew cask install adobe-acrobat-reader
+brew cask install vuze
+brew cask install dropbox
+brew cask install evernote
+# brew cask install firefox
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install google-backup-and-sync
+brew cask install skype
+brew cask install gimp
+brew cask install vlc
+brew cask install iterm2
+brew cask install expressvpn
+# brew cask install hyperdock
+# brew cask install divvy
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
+
